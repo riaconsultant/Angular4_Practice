6240ab189305007a096c381c914e50d77556a5c5
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-11-22 20:09:10 +0530

    refine code

--- mysociety/src/app/app.component.ts
+++ mysociety/src/app/app.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:"<h1>Hello World !!{{title + obj.name}}</h1>"
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-  obj = {name:"Manoj",age:3};
 }
