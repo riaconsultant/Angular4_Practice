957523aecc1b59f0cc0cac5299036e6b2902911b
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2017-12-11 10:35:32 +0530

    Auth Guard added

--- mysociety/src/app/app.route.ts
+++ mysociety/src/app/app.route.ts
@@ -6,11 +6,12 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { FeaturesComponent } from './features/features.component';
+import { AuthGuard } from './auth.guard';
 
 const appRoute: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent, canActivate: AuthGuard },
   { path: 'features', component: FeaturesComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: '**', component: PagenotfoundComponent}
