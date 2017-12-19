2fb2dc636621eee20025e439ef951098ccf97d81
Manoj Chaurasiya <manoj.chaurasiya@accionlabs.com>
2017-12-19 22:43:17 +0530

    common controller added

---
+++ mysociety/src/app/services/common.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http/src/http';
+
+@Injectable()
+export class CommonService {
+
+  constructor(private http:Http) { }
+
+  getStatic(){
+    
+  }
+
+}
